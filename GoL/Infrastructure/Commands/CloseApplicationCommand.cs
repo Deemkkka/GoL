92efd8e383850706103fd2718ec0b50b87d36111
Deem <Deem@DEEM>
2022-05-29 15:26:54 +0300

    Реализована кнопка закрытия приложения

---
+++ GoL/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using GoL.Infrastructure.Commands.Base;
+
+namespace GoL.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object paramater) => true;
+
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+    
+    }
+}
---
+++ GoL/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using GoL.ViewModels.Base;
+using GoL.Infrastructure.Commands;
+
+namespace GoL.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+
+        #region Команды
+
+        #region CloseApplicationCommand
+
+        public ICommand CloseApplicationCommand { get; }
+        private bool CanCloseApplicationExecute(object p) => true;
+
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+        #endregion
+
+
+        #endregion
+
+
+        public MainWindowViewModel()
+        {
+
+            #region Commands
+
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationExecute);
+
+            #endregion
+
+        }
+    }
+}
